8cda4d8b6d8eeb61b2ac7f7c1cee7a163df27798
vip-master <1998.illia.z@gmail.com>
2021-09-13 20:26:59 +0300

    reducer upgrade

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,19 +7,16 @@ import {
 	filter
 } from "./actions"
 
-const f=(state)=>({filter: state.filter ? state.filter : ""})
-
-
 export const reducer=createReducer({contacts:[], filter:""},{
-	[loadContacts]: (state,{payload})=>({...f(state), contacts: state.contacts ? [...state.contacts, ...payload] : [...payload]}),
-	[addContact]: (state,{payload})=>{
-		const contacts = state.contacts ? [...state.contacts] : []
+	[loadContacts]: ({filter, contacts: prevContacts},{payload})=>({filter, contacts: [...prevContacts, ...payload]}),
+	[addContact]: ({filter, contacts: prevContacts},{payload})=>{
+		const contacts = [...prevContacts]
 		contacts.push({id:uuidv4(), name:payload.name, number:payload.number, hidden:false})
-		return ({...f(state), contacts});
+		return ({filter, contacts});
 	},
-	[delContact]: (state,{payload})=>({...f(state), contacts: state.contacts ? state.contacts.filter(i=>i.id!==payload):[]}),
-	[filter]: (state,{payload})=>{
-		const filtered=state.contacts.map(i=>{
+	[delContact]: ({filter, contacts: prevContacts},{payload})=>({filter, contacts: prevContacts.filter(i=>i.id!==payload)}),
+	[filter]: ({contacts: prevContacts},{payload})=>{
+		const filtered=prevContacts.map(i=>{
 				const hidden = !i.name.toLowerCase().startsWith(payload.split(" ").join("").toLowerCase())
 				return {...i, hidden}
 			})
